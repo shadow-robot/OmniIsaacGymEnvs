a15d0b2cc7c5b991615a259d74ea6d088a03e3de
carebare47 <tom@shadowrobot.com>
2023-10-30 12:41:43 +0000

    fix tf2-ros package name

--- docker/ros.sh
+++ docker/ros.sh
@@ -5,7 +5,7 @@ apt install curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt update
 apt install -y ros-noetic-ros-base
-apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential tf2-ros
+apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential ros-noetic-tf2-ros
 apt install python3-rosdep
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source /opt/ros/noetic/setup.bash
